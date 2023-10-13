5b68ffe9f91804bc58c41fbba8595f4b0bd9d505
Lucas Tay <tayboonkai@gmail.com>
2023-10-14 01:29:01 +0800

    changing logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import pngtree from './pngtree.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pngtree} className="App-logo" alt="logo" />
         <p className= "App-title">
           Harmonia
         </p>
